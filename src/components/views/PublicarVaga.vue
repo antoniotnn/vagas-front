9c7f6bea1a93da1c477896d2d3d33911dd2b702e
Antonio Pires <tonan@outlook.com.br>
2022-02-14 06:11:22 -0300

    Criando os componentes Home e PublicarVaga

---
+++ src/components/views/PublicarVaga.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Publicar Vaga</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PublicarVaga'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
