7b9adb3e5dc8aa8e7b0016ca26aebd76feb6f5f8
Antonio Pires <tonan@outlook.com.br>
2022-02-14 07:10:10 -0300

    112. Ciclo de vida do componente (Lifecycle Hooks) - Introdução

--- src/components/layouts/Conteudo.vue
+++ src/components/layouts/Conteudo.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
     <h1>Componente Conteúdo</h1>
-    <p>Conteudo - Propriedade $style: {{ $style }}</p>
-    <div :class="$style.teste">Class teste-2</div>
-    <div :id="$style['teste-2']">ID teste-1</div>
     <home></home>
     <publicar-vaga></publicar-vaga>
   </div>
@@ -17,6 +14,48 @@ import PublicarVaga from '@/components/views/PublicarVaga.vue'
     components: {
       Home,
       PublicarVaga
+    },
+    methods: {
+
+    },
+    beforeCreate() {
+      console.log('Antes de criar');
+    },
+    created() {
+      console.log('Criado');
+    },
+    beforeMount() {
+      console.log('Antes de montar')
+    },
+    mounted() {
+      console.log('Montado')
+    },
+    beforeUpdate() {
+      console.log('Antes de atualizar')
+    },
+    updated() {
+      console.log('Atualizado')
+    },
+    beforeUnmount() {
+      console.log('Antes de desmontar/destruir')
+    },
+    unmounted() {
+      console.log('Desmontado/destruído')
+    },
+    errorCaptured() {
+      console.log('Erro capturado')
+    },
+    renderTracked() {
+      console.log('Re-renderização rastreada')
+    },
+    renderTriggered() {
+      console.log('Re-renderização ativada')
+    },
+    activated() {
+      console.log('Componente ativado')
+    },
+    deActivated() {
+      console.log('Componente desativado')
     }
   }
 </script>
