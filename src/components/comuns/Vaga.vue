d459929858073552a25742fcf9e5609e77a513bd
Antonio Pires <tonan@outlook.com.br>
2022-02-22 06:41:45 -0300

    124. Listando vagas

--- src/components/comuns/Vaga.vue
+++ src/components/comuns/Vaga.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="card">
-        <div class="card-header bg-dark text-white">Título da Vaga</div>
+        <div class="card-header bg-dark text-white">{{ titulo }}</div>
         <div class="card-body">
-            <p>Descrição truncada da vaga...</p>
+            <p>{{ descricao }}</p>
         </div>
         <div class="card-footer">
-            <small class="text-muted">Salário: R$ 6.000,00 | Modalidade: Home Office | Tipo: CLT | Publicação: 10/11/2021</small>
+            <small class="text-muted">Salário: R$ {{ salario }} | Modalidade: {{ modalidade }} | Tipo: {{ tipo }} | Publicação: {{ publicacao }}</small>
         </div>
     </div>
 </template>
@@ -13,6 +13,7 @@
 <script>
 
 export default {
-    name: 'Vaga'
+    name: 'Vaga',
+    props: ['titulo', 'descricao', 'salario', 'modalidade', 'tipo', 'publicacao']
 }
 </script>
