c06a9a8d7804f72c1f174f467c1287af51cc2341
Antonio Pires <tonan@outlook.com.br>
2022-02-22 06:29:03 -0300

    123. Criando o componente Vaga

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -7,6 +7,21 @@
     </div>
 
     <div class="row mt-5">
+      <div class="col-6">
+        <vaga />
+      </div>
+      <div class="col-6">
+        <vaga />
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col">
+        <vaga />
+      </div>
+    </div>
+
+    <div class="row mt-5">
       <div class="col col-4">
         <indicador titulo="Vagas abertas" :indicador="100" bg="bg-dark" color="text-white"></indicador>
       </div>
@@ -27,12 +42,14 @@
 
   import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue';
   import Indicador from '@/components/comuns/Indicador.vue';
+  import Vaga from '@/components/comuns/Vaga.vue';
 
   export default {
     name: 'Home',
     components: {
       PesquisarVaga,
-      Indicador
+      Indicador,
+      Vaga
     },
     data: () => ({
       usuariosOnline: 0
