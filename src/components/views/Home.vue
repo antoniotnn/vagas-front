2c960469b3702819b6e427ee93b9db5315e63e4f
Antonio Pires <tonan@outlook.com.br>
2022-02-22 05:56:18 -0300

    120. Comunicação entre componentes (props) - Pai para o filho

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -8,15 +8,15 @@
 
     <div class="row mt-5">
       <div class="col col-4">
-        <indicador></indicador>
+        <indicador titulo="Vagas abertas" indicador="100"></indicador>
       </div>
 
       <div class="col col-4">
-        <indicador></indicador>
+        <indicador titulo="Profissionais cadastrados" indicador="225"></indicador>
       </div>
 
       <div class="col col-4">
-        <indicador></indicador>
+        <indicador titulo="Visitantes online" indicador="25"></indicador>
       </div>
     </div>
   </div>
