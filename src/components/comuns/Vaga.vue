41ab3e00937a247e53c76cb0d25fc668c96cb71a
Antonio Pires <tonan@outlook.com.br>
2022-02-23 07:17:51 -0300

    134. Criando propriedades computadas para exibição da modalidade e tipo

--- src/components/comuns/Vaga.vue
+++ src/components/comuns/Vaga.vue
@@ -5,7 +5,7 @@
             <p>{{ descricao }}</p>
         </div>
         <div class="card-footer">
-            <small class="text-muted">Salário: R$ {{ salario }} | Modalidade: {{ modalidade }} | Tipo: {{ tipo }} | Publicação: {{ publicacao }}</small>
+            <small class="text-muted">Salário: R$ {{ salario }} | Modalidade: {{ getModalidade }} | Tipo: {{ getTipo }} | Publicação: {{ publicacao }}</small>
         </div>
     </div>
 </template>
@@ -51,6 +51,22 @@ export default {
             required: true
         }
     },
+    computed: {
+        getModalidade() {
+            switch(this.modalidade) {
+                case '1': return 'Home Office';
+                case '2': return 'Presencial';
+            }
+            return '';
+        },
+        getTipo() {
+            switch(this.tipo) {
+                case '1': return 'CLT';
+                case '2': return 'PJ';
+            }
+            return '';
+        }
+    }
     /*
     created () {
         console.log('tituloVagaTeste', typeof this.tituloVagaTeste);
