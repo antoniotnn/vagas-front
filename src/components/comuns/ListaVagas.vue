1391e7e67b0056d900edb39c8bab91ba9757390e
Antonio Pires <tonan@outlook.com.br>
2022-02-25 19:39:18 -0300

    159. Slots com escopo definido parte 2

--- src/components/comuns/ListaVagas.vue
+++ src/components/comuns/ListaVagas.vue
@@ -13,11 +13,24 @@ import Vaga from '@/components/comuns/Vaga.vue';
 
 export default {
     name: 'ListaVagas',
-    props: {
-        vagas: Array
-    },
+    data: () => ({
+        vagas: []
+    }),
     components: {
         Vaga
+    },
+    activated() { // método chamado sempre q um componente é ativado ou reativado
+      this.vagas = JSON.parse(localStorage.getItem('vagas'));
+    },
+    mounted() { //utilizou o mount e nao o activated, pois o componente esta dentro de um keep alive, entao mesmo quando ele for desativado, na primeira montagem o componente ja tera sido configurado como escutador do evento, e esssa escuta continuara funcionando
+      this.emitter.on('filtrarVagas', vaga => {
+        //console.log('Estamos no componente Home', vaga);
+
+        const vagas = JSON.parse(localStorage.getItem('vagas'));
+
+        //console.log(vagas);
+        this.vagas = vagas.filter(reg => reg.titulo.toLowerCase().includes(vaga.titulo.toLowerCase())); //Cria um novo array com todos os elementos que atenderem a uma condicao
+      });
     }
 }
 </script>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -6,7 +6,7 @@
       </div>
     </div>
 
-    <lista-vagas :vagas="vagas" />
+    <lista-vagas />
 
     <div class="row mt-5">
       <div class="col col-4">
@@ -40,7 +40,6 @@
     },
     data: () => ({
       usuariosOnline: 0,
-      vagas: []
     }),
     methods: {
       getUsuariosOnline() {
@@ -50,20 +49,6 @@
     created() {
       setInterval(this.getUsuariosOnline, 1000) //declarar this.getUsuariosOnline() com o parêntese irá passar o resultado da funçao. Sem o parêntese indica que é pra executar a função.
       // a cada 1 segundo executar a função.
-    },
-    //mounted() { //chamado uma única vez quando o componente é montado, gera um problema pois como o keep alive está ligado, quando o componente for reativado , ele não é montado novamente, logo não recarrega atualizacoes, portanto foi substituido pelo activated, que é chamado sempre q o componente for reativado
-    activated() { // método chamado sempre q um componente é ativado ou reativado
-      this.vagas = JSON.parse(localStorage.getItem('vagas'));
-    },
-    mounted() { //utilizou o mount e nao o activated, pois o componente esta dentro de um keep alive, entao mesmo quando ele for desativado, na primeira montagem o componente ja tera sido configurado como escutador do evento, e esssa escuta continuara funcionando
-      this.emitter.on('filtrarVagas', vaga => {
-        //console.log('Estamos no componente Home', vaga);
-
-        const vagas = JSON.parse(localStorage.getItem('vagas'));
-
-        //console.log(vagas);
-        this.vagas = vagas.filter(reg => reg.titulo.toLowerCase().includes(vaga.titulo.toLowerCase())); //Cria um novo array com todos os elementos que atenderem a uma condicao
-      });
     }
   }
 </script>
